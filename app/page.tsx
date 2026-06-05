570521da705ba7dd54c737acef3ccbcb3c164785
477heat <477heat@gmail.com>
2026-06-04 22:57:27 -0600

    Update launch countdown date

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import { useEffect, useRef, useState } from "react";
 import { GlossaryText } from "@/components/GlossaryTerm";
 import type { GlossaryTermKey } from "@/lib/glossary";
 
-const dayOneLaunchAt = Date.UTC(2026, 5, 5, 23, 0, 0);
+const dayOneLaunchAt = Date.UTC(2026, 5, 8, 12, 0, 0);
 const homeGlossaryTerms: GlossaryTermKey[] = [
   "Access Token",
   "Actual Supply",
